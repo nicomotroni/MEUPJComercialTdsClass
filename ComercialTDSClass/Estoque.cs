47ded5dcda9077814066747f16ec66a99ceb81be
Nicole Motroni <nicolemotroni@gmail.com>
2025-05-21 11:56:44 -0300

    últimas atualizações

--- ComercialTDSClass/Estoque.cs
+++ ComercialTDSClass/Estoque.cs
@@ -15,15 +15,20 @@ namespace ComercialTDSClass
         public Categoria? Categoria { get; set; }
         public DateTime DataCard { get; set; }
         public byte[] imagem { get; set; }
-        public string? Nome; { get; set; }
-
-
+        public string? Nome { get; set; }
+        public Estoque()
+        {
+            Categoria = new();
+        }
 
     }
 }
-/* quando for inserir uma imagem no C#
+/*quando for inserir uma imagem no C#
 o símbolo "[]" não quer dizer que contém uma imagem ali, 
 mas que esta sendo convertido em bytes uma 
 matriz de bytes que indica que pode ser inserido imagem 
-para o c# consiga interpretar/rodar aquela informação*/
+para o c# consiga interpretar/rodar aquele dado*/
+ public Estoque(int id, string? codBarras, string? descricao, double valorunit, Categoria? categoria, DateTime datacard, byte[] imagem, string nome)
+{
 
+}
