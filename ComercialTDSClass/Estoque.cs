4d03e6daef0339ed57931ad2bdead97b91786dc0
Nicole Motroni <nicolemotroni@gmail.com>
2025-06-10 11:24:15 -0300

    atualização estoque

--- ComercialTDSClass/Estoque.cs
+++ ComercialTDSClass/Estoque.cs
@@ -39,4 +39,4 @@ namespace ComercialTDSClass
 
         // os métodos não estão especificados na construção dele
         //precisa retirar alguns contrutores que não estão bem especificados 
-        //atualizar aqui 10/06 inserir o estoque 
+        //atualizar aqui 10/06 inserir o estoque
